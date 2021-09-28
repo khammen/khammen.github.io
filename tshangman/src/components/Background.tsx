383164161308666d9ecdacc9bc2f64bb53c4175b
Kurt Hammen <khammenscout@gmail.com>
2021-09-28 15:05:27 -0400

    Set up React Boostrap with tshangman

---
+++ tshangman/src/components/Background.tsx
@@ -0,0 +1,13 @@
+import Hang0 from './Images/Hang0.jpg';
+import Hang1 from './Images/Hang1.jpg';
+import Hang2 from './Images/Hang2.jpg';
+import Hang3 from './Images/Hang3.jpg';
+import Hang4 from './Images/Hang4.jpg';
+import Hang5 from './Images/Hang5.jpg';
+import HangGameOver from './Images/HangGameOver.jpg';
+
+export function Background(): JSX.Element{
+    return <div>
+        <img src={Hang0}/>
+    </div>
+}
