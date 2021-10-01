7d659ea6e1901a190a2d7ac64d1b16f9d57ac8ca
Kurt Hammen <khammenscout@gmail.com>
2021-09-30 21:52:53 -0400

    Functional version of hangman! Huzzahgit add -A!

--- tshangman/src/components/Menu.tsx
+++ tshangman/src/components/Menu.tsx
@@ -18,11 +18,10 @@ export function Menu({lossLevel, setLossLevel, guessedLetters, setGuessedLetters
       const tempWord = window.prompt("Enter a word for someone else to guess:");
       setHiddenWord(tempWord as string);
       setBlanks('');
-      displayBlanks(hiddenWord, entryBoxText);
+      initializeBlanks();
     }
     function guessBox(boxText: string){
       if (hiddenWord.includes(entryBoxText)){
-        setBlanks('');
         displayBlanks(hiddenWord, entryBoxText);
       } else {
         setGuessedLetters(guessedLetters + entryBoxText);
@@ -30,15 +29,28 @@ export function Menu({lossLevel, setLossLevel, guessedLetters, setGuessedLetters
       }
       setEntryBoxText('');
     }
-    function displayBlanks(hiddenWord: string, guessedLetters: string): void{
+    function displayBlanks(hiddenWord: string, entryBoxText: string): void{
+      let tempString = '';
       for (let i=0; i<hiddenWord.length; i++){
-          if (hiddenWord[i]===guessedLetters){
-              setBlanks(blanks + hiddenWord[i] + "  ");
+          if (hiddenWord[i]==entryBoxText[0]){
+            tempString = tempString + entryBoxText[0] + "  ";
           } else {
-              setBlanks(blanks + "_  ");
+            if (hiddenWord[i]==blanks[(3*i)]){
+              tempString = tempString + hiddenWord[i] + "  ";
+            } else {
+              tempString = tempString + "_  ";
+            }
           }
       }
-  }
+      setBlanks('');
+      setBlanks(tempString);
+    }
+    function initializeBlanks(){
+      let temp = '';
+      for (let i=0; i<hiddenWord.length; i++){
+        temp = temp + "_  ";
+      }
+    }
 
     return <div><form>
     <input
